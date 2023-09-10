9526d9b92fe3116b9043a7bd3406d78992b72be8
irakli <112957853+irakli182@users.noreply.github.com>
2023-09-10 15:44:24 -0700

    Update example-ui.tsx

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from "next";
-import { MetaHeader } from "~~/components/MetaHeader";
-import { ContractData } from "~~/components/example-ui/ContractData";
-import { ContractInteraction } from "~~/components/example-ui/ContractInteraction";
 
 const ExampleUI: NextPage = () => {
   return (

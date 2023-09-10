f262d4140fabb50b9e3f8dc23a766329335bfb30
irakli <112957853+irakli182@users.noreply.github.com>
2023-09-10 16:17:01 -0700

    Update example-ui.tsx

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -1,17 +1,55 @@
 import type { NextPage } from "next";
+import React, { useState } from "react";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+
 
 const ExampleUI: NextPage = () => {
+
+
+  // getting values from input (address and token amount)
+
+  const [addr, setAddr] = useState<string>('');
+  const [amount, setAmount] = useState<string>('');
+
+  const handleAddrChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAddr(event.target.value);
+  };
+  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(event.target.value);
+  };
+
+
+  const { writeAsync, isMining } = useScaffoldContractWrite({
+    contractName: "NNNToken", // Replace with the actual contract name
+    functionName: "mint", // Replace with the actual minting function name
+    args: [] // Initialize with empty arguments
+    // Additional configuration options if needed
+  });
+
+  const mintTokens = async () => {
+    console.log('MINT NNN Token!');
+    const IntAmount = Number(amount);
+    const weiAmount = IntAmount * 10**18;
+    const weiAmountBigInt = BigInt(weiAmount);
+    // Call the minting function with the updated arguments
+    await writeAsync({
+      args: [addr, weiAmountBigInt], // Provide the recipient's address and weiAmount as bigint
+      // Additional configuration options if needed
+    });
+  }
+
+
   return (
     <>
      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
         <p>address:</p>
-        <input placeholder="wallet address" id="addr" style={{color: "black", width: '200px'}}/>
+        <input placeholder="wallet address" id="addr" style={{color: "black", width: '200px'}} onChange={handleAddrChange}/>
         <br></br>
         <p>amount</p>
-        <input placeholder="NNN token amount" id="amount" style={{color: 'black', width: '200px'}}/>
+        <input placeholder="NNN token amount" id="amount" style={{color: 'black', width: '200px'}} onChange={handleAmountChange} />
         <br></br>
 
-        <button style={{color: 'white', width: '50px', backgroundColor: 'purple', borderRadius: '10px', padding: '5px'}}
+        <button onClick={mintTokens} style={{color: 'white', width: '50px', backgroundColor: 'purple', borderRadius: '10px', padding: '5px'}}
 
         >Mint</button>
       </div>

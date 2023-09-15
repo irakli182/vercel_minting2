a15375ad0fdec13e72242d16ba95be7d76c25d59
irakli <112957853+irakli182@users.noreply.github.com>
2023-09-15 05:56:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
 
-const network = process.env.YARN_ENABLE_IMMUTABLE_INSTALLS === false ? 'production' : 'staging';
+const network = process.env.YARN_ENABLE_IMMUTABLE_INSTALLS;
 
 export default network;
